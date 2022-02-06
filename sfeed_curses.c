1594ee5f0e8bf43e3cd122719be9c81fcf26a897
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-02-06 11:25:36 +0100

    fix a warning with tparm on some systems
    Older POSIX standards also defined it as a char * parameter (not const char *)
    for tparm().
    
    https://pubs.opengroup.org/onlinepubs/7908799/xcurses/term.h.html

--- sfeed_curses.c
+++ sfeed_curses.c
@@ -267,8 +267,8 @@ estrdup(const char *s)
 
 /* Wrapper for tparm which allows NULL parameter for str. */
 char *
-tparmnull(const char *str, long p1, long p2, long p3, long p4, long p5,
-          long p6, long p7, long p8, long p9)
+tparmnull(char *str, long p1, long p2, long p3, long p4, long p5, long p6,
+          long p7, long p8, long p9)
 {
 	if (!str)
 		return NULL;
